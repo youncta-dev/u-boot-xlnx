b25fecdf4604e0a4da48f0e14dada7d0399ec4e1
Antonio Di Bacco <antonio.dibacco@youncta.com>
2017-06-26 18:49:01 +0200

    support for sampling clock set to 100 or 125

--- board/xilinx/zynq/ypacket2_hw_platform/si5347_config.h
+++ board/xilinx/zynq/ypacket2_hw_platform/si5347_config.h
@@ -9,10 +9,17 @@ typedef struct
 
 } si5347ab_register_t;
 
+#define SAMPLES_CLK_125
 
-#include "./si5347/Si5347_revD_samples100_serdes125_run3-Registers.h"
-#include "./si5347/Si5347_revB_samples100_serdes125_run3-Registers.h"
+#ifdef SAMPLES_CLK_100
+#include "./si5347/Si5347_revD_adc100_dac100_serdes125_run3-Registers.h"
+#include "./si5347/Si5347_revB_adc100_dac100_serdes125_run3-Registers.h"
+#endif
 
+#ifdef SAMPLES_CLK_125
+#include "./si5347/Si5347_revD_adc125_dac250_serdes125_run3-Registers.h"
+#include "./si5347/Si5347_revB_adc125_dac250_serdes125_run3-Registers.h"
+#endif
 
 #endif
 
